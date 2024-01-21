be303bda622a05ec949cbf0005f95e6a4a99f41c
JCYhapon <yhaponjc@gmail.com>
2024-01-21 15:51:03 +0800

    update: add ID on hero section

--- src/components/sections/hero.jsx
+++ src/components/sections/hero.jsx
@@ -1,7 +1,11 @@
 import React from "react";
 
 const hero = () => {
-  return <section className="min-h-[84vh] lg:min-h-screen">Hero</section>;
+  return (
+    <section id="about" className="min-h-[84vh] lg:min-h-screen">
+      Hero
+    </section>
+  );
 };
 
 export default hero;
