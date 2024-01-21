37d92cc73bdfa70e6b0c1af30ecc834f393762a8
JCYhapon <yhaponjc@gmail.com>
2024-01-21 12:34:28 +0800

    import: all the components

---
+++ src/components/sections/import.js
@@ -0,0 +1,7 @@
+import contact from "./contact";
+import footer from "./footer";
+import hero from "./hero";
+import projects from "./projects";
+import skills from "./skills";
+
+export { contact, footer, hero, projects, skills };
