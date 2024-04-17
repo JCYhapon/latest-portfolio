b870361093bd463fc8fd8ebe22e460a095440de3
JCYhapon <yhaponjc@gmail.com>
2024-04-17 23:06:25 +0800

    removed space above

--- src/components/sections/hero.jsx
+++ src/components/sections/hero.jsx
@@ -7,7 +7,7 @@ const hero = () => {
   return (
     <section
       id="about"
-      className="w-[60%] lg:min-h-[80vh] container mx-auto flex flex-row items-center justify-center"
+      className="w-[60%] lg:min-h-[80vh] container mx-auto flex flex-row items-center justify-center mt-[-6rem]"
     >
       <div className="flex items-center gap-5">
         <div className="bottom-5">
